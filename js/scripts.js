5c419f9c599f6e8ef7662195312ff288d64deba3
Meghan Lindsley & Aoede Pando <student@epicodus.com>
2014-07-14 11:26:49 -0700

    final spec and function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 function scrabble(letter) {
-  var input = letter;
-  var dictionary ={"c": 3};
+  var input = letter.toLowerCase().split("");
+  var dictionary ={"b": 3, "c":3, "m":3, "p":3, "a":1, "e":1, "i":1, "o":1, "u":1, "l":1, "n":1, "r":1, "s":1, "t":1, "d":2, "g":2, "f":4, "h":4, "v":4, "w":4, "y":4, "k":5, "j":8, "x":8, "q":10, "z":10};
   var value = 0;
 
+  for (var i = 0; i < input.length; i++)  {
+    value += dictionary[input[i]];
+  }
 
-
-   return dictionary[letter];
+   return value;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,7 @@ describe("scrabble", function() {
   it("takes a letter and returns a number value", function() {
     scrabble("c").should.equal(3);
   });
+  it("takes a word and returns a number value", function()  {
+    scrabble("yoga").should.equal(8);
+  });
 });
